a5a43417cc56831754a949e92430f4693bfe71f9
letsgitit24 <jacobsavoie33@gmail.com>
2021-07-12 01:47:56 -0400

    Updated Icon bar/links

---
+++ script.js
@@ -0,0 +1,7 @@
+function newGitWindow(){
+    window.open("https://github.com/letsgitit24")
+} //Opens Github in a new window
+
+function newLinkedInWindow(){
+    window.open("https://www.linkedin.com/in/jacob-savoie")
+} // Opens LinkedIn in a new window

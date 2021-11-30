3794cdb06efb75aa9ebada94dc9853d7c37b6938
letsgitit24 <jacobsavoie33@gmail.com>
2021-11-29 23:12:05 -0500

    cgs

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function newGitWindow(){
 function newLinkedInWindow(){
     window.open("https://www.linkedin.com/in/jacob-savoie")
 } // Opens LinkedIn in a new window
+
+function newConsultation(){
+  window.open("mailto:jacobsavoie33@gmail.com")
+} // Allows for user to send an email
